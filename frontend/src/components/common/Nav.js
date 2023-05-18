ed0b4e61430a2307651b9def525b99ac61dd5c41
Neil-McCarthy <73938609+Neil-McCarthy@users.noreply.github.com>
2023-05-18 14:29:06 +0100

    create auth token on login/remove on logout

--- frontend/src/components/common/Nav.js
+++ frontend/src/components/common/Nav.js
@@ -1,6 +1,28 @@
-import {Link} from 'react-router-dom';
+import { useEffect } from 'react'
+import {Link, useNavigate} from 'react-router-dom';
+import { useSendLogoutMutation } from '../../features/auth/authApiSlice';
+
 
 const Nav = () => {
+
+    const navigate = useNavigate()
+
+    const [sendLogout, {
+        isLoading,
+        isSuccess,
+        isError,
+        error
+    }] = useSendLogoutMutation()
+
+    useEffect(() => {
+        if (isSuccess) navigate('/')
+    }, [isSuccess, navigate])
+
+    if (isLoading) return <p>Logging Out...</p>
+
+    if (isError) return <p>Error: {error.data?.message}</p>
+
+
     return (
         <nav>
             <ul>
@@ -8,6 +30,7 @@ const Nav = () => {
                 <Link to='/'>Trinkets</Link>
                 <Link to='/users/newUser'>Sign Up</Link>
                 <Link to='/login'>Login</Link>
+                <button onClick={sendLogout}>Logout</button>
                 <Link to='/users/usersList'>All users</Link>
             </ul>
         </nav>

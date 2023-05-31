ab07d18b8dfab92bd590bdda3397f996480261e3
Neil-McCarthy <73938609+Neil-McCarthy@users.noreply.github.com>
2023-05-31 11:27:01 +0100

    take site down and reset to local - working

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
-    'https://trinkets-for-trade.onrender.com'
+    // 'https://trinkets-for-trade.onrender.com'
+    'http://localhost:3000'
 ]
 
 module.exports = allowedOrigins;
